ed21a415a0550c54d98ac94849bdcc5e82bb9d39
wuluwululang <38177060+wuluwululang@users.noreply.github.com>
2019-03-05 20:56:22 +0800

    update: add task to lib

--- lua_async_await_support.lua
+++ lua_async_await_support.lua
@@ -1,22 +1,34 @@
 local M = {}
-local DEBUG_MODE = false
+local DEBUG_MODE = true
 local log = function(...)
     if(DEBUG_MODE)then
         print(...)
     end
 end
-M.async = function(func)
-    return function(value)
-        return function(waiter)
+log('DEBUG_MODE OPEN')
+local task = {
+    new = function(func)
+        return setmetatable({__ori = func, __type = 'task'},{
+            __call = function(t,...)
+                return t.__ori(...)
+            end
+        })
+    end
+}
+local m = {
+    __call = function(t,value)
+        --return a task
+        return task.new(function(waiter)
             local co
             local resume = function(...)
                 return coroutine.resume(co,...)
             end
+            local func = t.__ori
             setfenv(func, setmetatable({
                 await = function(p)
                     log("await co: ",co)
                     log(p)
-                    if(type(p)=='function')then
+                    if(type(p)=='table' and p.__type=='task')then
                         p{
                             onSuccess = resume,
                             onError = function(e)
@@ -38,8 +50,15 @@ M.async = function(func)
                 waiter.onSuccess(ret)
             end)
             resume()
-        end
+        end)
     end
+}
+
+M.task = task
+
+M.async = function(func)
+    log('async')
+    return setmetatable({__type = 'asyncfunction', __ori = func}, m)
 end
 
 M.await = function(task)
